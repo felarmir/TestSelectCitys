d9ffb9abc285cd12136b704cee754a4dd2977bdd
Denis <wiirlock@gmail.com>
2016-06-08 21:47:53 +0300

    Initial Commit

---
+++ TestSelectCitys/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestSelectCitys
+//
+//  Created by Denis Andreev on 08.06.16.
+//  Copyright © 2016 Denis Andreev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
